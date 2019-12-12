f28617903f0e65e13f0bae2ac63d08d6cce733ee
hybedot <olubiyi.ibrahim.a@gmail.com>
2019-12-12 22:57:58 +0100

    modification-1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
 from flask_cors import CORS
+from sklearn.externals import joblib
 import numpy as np
 import pickle as p
 
@@ -12,19 +13,25 @@ CORS(app)
 
 api = Api(app)
 
-modelfile = 'models/car_prediction.pickle'
-model = p.load(open(modelfile, 'rb'))
+modelfile = 'models/recuit.pkl'
+model = joblib.load(modelfile)
 
 class Predict(Resource):
     def post(self):
-        json_data = request.get_json()
-        
-        try:
-            condition = json_data['condition']
-            mileage = json_data['mileage'] 
-            year = json_data['year']
 
-            prediction_data = [condition, mileage, year]
+        try:
+            json_data = request.get_json()
+
+            qualification = json_data['qualification']
+            target_met = json_data['target_met'] 
+            foreign_schooled = json_data['foreign_schooled']
+            previous_award = json_data['previous_award']
+            class_of_degree = json_data['class_of_degree']
+            interview_test = json_data['interview_test']
+            fluency_confidence = json_data['fluency_confidence']
+
+            prediction_data = [qualification, target_met, foreign_schooled, previous_award, class_of_degree
+                                interview_test, fluency_confidence]
 
             prediction = np.array2string(model.predict([prediction_data]))
 
@@ -42,9 +49,16 @@ class Predict(Resource):
                     }, 500
 
 
+class Homepage(Resource):
+    def get(self):
+        return 'Hello'
+
+
+
 
 #connect class with endpoint
 api.add_resource(Predict, '/api/predict')
+api.add_resource(Homepage, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
